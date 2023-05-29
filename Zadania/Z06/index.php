34d1af05473a4f04bc071aac24522553052a3dfd
Domin122 <dgabrysz3@gmail.com>
2023-05-29 10:29:47 +0200

    commit

--- Zadania/Z06/index.php
+++ Zadania/Z06/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Zadanie 06 </title>
+    <title>Zad06 </title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -14,12 +14,15 @@
     <h2>Autor: Dominik Gabrysz 2ir_2</h2>
 </header>
 <section>
-    <p>Napisz program, który oblicza sumę, różnicę, iloczyn i iloraz (całkowity - jak w C++) liczb całkowitych A i B.</p>
-    <form action="z05w.php" method="post">
+    <p>Napisz program, który oblicza pole trójkąta na podstawie długości jego boków za pomocą
+        <a href="https://pl.wikipedia.org/wiki/Wz%C3%B3r_Herona">wzoru Herona</a></p>
+    <form action="z06w.php" method="post">
         <label for="a">Podaj a:</label>
         <input type="text" id="a" name="a">
         <label for="b">Podaj b:</label>
         <input type="text" id="b" name="b">
+        <label for="c">Podaj c:</label>
+        <input type="text" id="c" name="c">
         <input type="submit" value="Wyślij">
     </form>
 </section>
